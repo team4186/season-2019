c065c0ee7ac66b82cffe195423bcaad8e5ad6ee3
Nikhil Parikh <nikhilcjparikh@gmail.com>
2019-11-19 14:43:14 -0800

    JankyPIDs
    Zak, you can use these for reference but try to code it on your own :). Added possible failsafe code, we will have to test it to see if it actually is useful or if it makes our robot into a veggietale.

---
+++ pwm-westcoast/src/main/java/frc/commands/PIDDriveADIS.java
@@ -0,0 +1,78 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.analog.adis16448.frc.ADIS16448_IMU;
+
+public class PIDDriveADIS extends Command {
+  private final Joystick joystick;
+  private final ADIS16448_IMU adis;
+  private final PIDController pid;
+  double p = 0.05;
+  double i = 0.02;
+  double d = 0.03;
+  double wait = 0;
+  
+  public PIDDriveADIS(
+    DifferentialDrive drive,
+    Joystick joystick,
+    ADIS16448_IMU adis
+  ) {
+    this.joystick = joystick;
+    this.adis = adis;
+
+    this.pid = new PIDController(p, i, d, adis, (turnfactor) -> drive.arcadeDrive(joystick.getY(), turnfactor));
+  }
+
+  @Override
+  protected void initialize() {
+    adis.reset();
+    pid.reset();
+    pid.setContinuous(false);
+    pid.setAbsoluteTolerance(0.5);
+    pid.setInputRange(-6, 6);
+    pid.setOutputRange(-1,1);
+    pid.setPIDSourceType(PIDSourceType.kRate);
+    pid.enable();
+  }
+
+  @Override
+  protected void execute() {
+    pid.setSetpoint(joystick.getTwist()*6);
+
+    if(adis.getRateZ() >= 6){
+      pid.reset();
+    }
+    else if (adis.getRateZ() <= -6){
+      pid.reset();
+    }
+    else if (adis.getRateZ() == 0){
+      wait = wait + 1;
+      if (wait == 5){
+        pid.reset();
+        wait = -50;
+      }
+    }
+    if(wait <= 0){
+      wait = wait + 1;
+    }
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  protected void end() {
+    pid.disable();
+  }
+
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
--- pwm-westcoast/src/main/java/frc/robot/Robot.java
+++ pwm-westcoast/src/main/java/frc/robot/Robot.java
@@ -1,19 +1,28 @@
 package frc.robot;
 
+import com.analog.adis16448.frc.ADIS16448_IMU;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.commands.PIDDriveADIS;
 
 public class Robot extends TimedRobot {
 
   //Input
   private final Joystick joystick = new Joystick(0);
+
+  //Sensors
+  private final ADIS16448_IMU adis = new ADIS16448_IMU();
   
   //Drivetrain
   private final Talon leftTalons = new Talon(0);
   private final Talon rightTalons = new Talon(1);
   private DifferentialDrive drive = new DifferentialDrive(leftTalons, rightTalons);
+
+  //Commands
+  PIDDriveADIS PIDdrive = new PIDDriveADIS(drive, joystick, adis);
 
   //Extra Actuators
   private final Talon actuator1 = new Talon(2);
@@ -42,7 +51,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    
+    PIDdrive.cancel();
+    PIDdrive.start();
   }
 
   @Override
